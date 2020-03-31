a53f783311941abc4b5b8047434c94d532dc2fc7
Dumitru Liviu <Liviu_liviutzu99@yahoo.com>
2020-03-31 23:22:22 +0300

    Lab3

---
+++ lab-1.1/src/ro/unitbv/IfElseProblem/IfElseProblem.java
@@ -0,0 +1,102 @@
+package ro.unitbv.IfElseProblem;
+
+public class IfElseProblem {
+
+//	!!!!!       Doua ce arti terminat de srics codul formatati fisierul cu Ctrl+shift+f
+
+	public static void main(String[] args) {
+
+		// System.out.println(getMaxSpeedLimitByIfElse("Drum Expres"));
+		// System.out.println(getMaxSpeedLimitByIfElse("Oras"));
+		// System.out.println(getMaxSpeedLimitByIfElse("Autostrada"));
+		// System.out.println(getMaxSpeedLimitByIfElse("Necunoscut"));
+
+		//System.out.println(getMaxSpeedLimitByIfElseComplex("Drum Expres"));
+		//System.out.println(getMaxSpeedLimitByIfElseComplex("Oras"));
+		//System.out.println(getMaxSpeedLimitByIfElseComplex("Autostrada"));
+		//System.out.println(getMaxSpeedLimitByIfElseComplex("Necunoscut"));
+		
+		System.out.println(getMaxSpeedLimitBySwitch("Oras"));
+		System.out.println(getMaxSpeedLimitBySwitch("Autostrada"));
+		System.out.println(getMaxSpeedLimitBySwitch("Zona Rezidentiala"));
+		System.out.println(getMaxSpeedLimitBySwitch("Drum European"));
+		
+
+	}
+
+	public static int getMaxSpeedLimitByIfElse(String roadType) {
+		if (roadType == "Autostrada") {
+			System.out.println("Viteza maxima pe Autostrada=");
+			return 130;
+		} else if (roadType == "Zona Rezidentiala") {
+			System.out.println("Viteza maxima pe Zona Rezidentiala=");
+			return 30;
+		} else if (roadType == "Drum Expres") {
+			System.out.println("Viteza maxima pe Drum Expres=");
+			return 100;
+		} else if (roadType == "Oras") {
+			System.out.println("Viteza maxima in Oras=");
+			return 50;
+		} else if (roadType != "Drum Expres" && roadType != "Oras" && roadType != "Autostrada"
+				&& roadType != "Zona Rezidentiala") {
+			System.out.println("Drumul este necunoscut=");
+		}
+
+		return 0;
+	}
+
+	public static int getMaxSpeedLimitByIfElseComplex(String roadType) {
+
+		if (roadType == "Autostrada") {
+			System.out.println("Viteza maxima pe Autostrada=");
+			return 130;
+
+		} else {
+			if (roadType == "Zona Rezidentiala") {
+				System.out.println("Viteza maxima in Zona Rezidentiala=");
+				return 30;
+			} else {
+				if (roadType == "Drum Expres") {
+					System.out.println("Viteza maxima pe Drum Expres=");
+					return 100;
+				} else {
+					if (roadType == "Oras") {
+						System.out.println("Viteza maxima in Oras=");
+						return 50;
+					} else {
+						if (roadType != "Autostrada" && roadType != "Oras" && roadType != "Drum Expres"
+								&& roadType != "Zona Rezidentiala") {
+
+							System.out.println("Drumul este necunoscut=");
+						}
+					}
+				}
+
+			}
+		}
+		return 0;
+
+	}
+
+	public static int getMaxSpeedLimitBySwitch(String roadType) {
+		switch (roadType) {
+		case "Autostrada":
+			System.out.println("Vitza maxima pe Autostrada=");
+			return 130;
+		case "Zona Rezidentiala":
+			System.out.println("Viteza maxima in Zona Rezidentiala=");
+			return 30;
+		case "Drum Expres":
+			System.out.println("Viteza maxima pe Drum Expres");
+			return 100;
+		case "Oras":
+			System.out.println("Viteza maxima in oras");
+			return 50;
+		default:
+			System.out.println("Drum Necunoscut");
+		}
+
+		return 0;
+	}
+
+}
---
+++ lab-1.1/src/ro/unitbv/Sir/Sir.java
@@ -0,0 +1,51 @@
+package ro.unitbv.Sir;
+
+import java.util.Arrays;
+
+public class Sir {
+
+	public static void main(String[] args) {
+		double[] sir = { 30, 40, 33, 44, 38.5, 41.1, 33.3 };
+		int index1 = 0;
+		for (int j = 0; j < sir.length; j++) {
+			index1++;
+		}
+		double[] siri = new double[index1];
+		for (int j = 0; j < sir.length; j++) {
+			siri[j] = sir[j];
+		}
+		System.out.println("Sirul initial" + Arrays.toString(siri));
+		Arrays.sort(sir);
+		int i = 0;
+		while (i < sir.length & sir[i] < 37) {
+			i++;
+		}
+		double[] Temperaturinormale = new double[i];
+		double[] Temperaturianormale = new double[sir.length - i];
+		for (int j = 0; j < Temperaturinormale.length; j++) {
+			Temperaturinormale[j] = sir[j];
+		}
+		for (int j = 0; j < Temperaturianormale.length; j++) {
+			Temperaturianormale[j] = sir[j + i];
+		}
+		System.out.println("Temperaturile normale sunt=" + Arrays.toString(Temperaturinormale));
+		for (int j = 0; j < Temperaturinormale.length; j++) {
+			for (int z = 0; z < siri.length; z++) {
+				if (siri[z] == Temperaturinormale[j])
+					System.out.println("Indexul pentru=" + Temperaturinormale[j] + " este=" + z);
+
+			}
+
+		}
+		System.out.println("\n");
+		System.out.println("Temperaturile anormale sunt=" + Arrays.toString(Temperaturianormale));
+		for (int j = 0; j < Temperaturianormale.length; j++) {
+			for (int z = 0; z < siri.length; z++) {
+				if (siri[z] == Temperaturianormale[j])
+					System.out.println("Indexul pentru=" + Temperaturianormale[j] + " este=" + z);
+
+			}
+		}
+	}
+
+}
