b2c4919090ae646557dde12d901542d788ba0f84
Dumitru Liviu <Liviu_liviutzu99@yahoo.com>
2020-04-01 15:48:30 +0300

    Lab3

--- lab-1.1/src/ro/unitbv/clase/Clase.java
+++ lab-1.1/src/ro/unitbv/clase/Clase.java
@@ -3,15 +3,19 @@ package ro.unitbv.clase;
 public class Clase {
 
 	public static void main(String[] args) {
-		Customer customer = new Customer("Dumitru", "Jalba", 2);
+		
 		Adress ad = new Adress("Petuniei", "Brasov", 2);
 		System.out.println(ad);
+		Adress ad1 = new Adress("Cina", "Ploiesti", 1);
+		System.out.println(ad);
 		PaymentMethod PM = new PaymentMethod("Card");
+		PaymentMethod PM1 = new PaymentMethod("Cash");
+		Customer customer = new Customer("Dumitru", "Jalba", 18, ad1, PM);
 		System.out.println(PM);
+		System.out.println(PM1);
 		System.out.println(customer);
-		Customer customer1 = new Customer("Liviu", "Dumitru", 21, ad, PM);
+		Customer customer1 = new Customer("Liviu", "Dumitru", 21, ad, PM1);
 		System.out.println(customer1);
-
 		System.out.println(customer1.getPaymentMethod().getType());
 		System.out.println(customer1.getCompleteName());
 		System.out.println(customer1.getAdress().getNumber());
